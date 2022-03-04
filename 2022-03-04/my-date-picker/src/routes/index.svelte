f29b0c69f5d4bd7aac4c3204bfbda0849321677f
andrew_wang <andrew_wang@friday.tw>
2022-03-04 16:56:59 +0800

    [2022-03-04] add prev / next icon

--- 2022-03-04/my-date-picker/src/routes/index.svelte
+++ 2022-03-04/my-date-picker/src/routes/index.svelte
@@ -1,83 +1,122 @@
 <script>
-  let date = new Date();
-  let format = 'YYYY-MM-DD';
-  let viewMode = 'days';
+    import moment from "moment";
+
+    let selectDate = moment().startOf('day');
+    let format = 'YYYY-MM-DD';
+    let viewMode = 'days'; // 有 'times' . 'days' . 'months' . 'years' . 'decades'
+    let viewDate = selectDate ? moment(selectDate).startOf('month') : moment();
+
+    function minusMonthToViewDate() {
+        viewDate = moment(viewDate).subtract(1, 'month');
+    }
+
+    function addMonthToViewDate() {
+        viewDate = moment(viewDate).add(1, 'month');
+    }
+
+    function getDateArr(viewDate) {
+
+        const row = 6;
+        const column = 7;
+
+        const firstDate = moment(viewDate).date(1).startOf('week');
+        const dateArr = [];
+        dateArr.push(firstDate);
+
+        while (dateArr.length < row * column) {
+
+            const newDate = moment(dateArr[dateArr.length - 1]).add(1, 'day');
+            dateArr.push(newDate);
+        }
+
+        return dateArr;
+    }
+
+    function getDayClass(selectDate,day) {
+
+        if (selectDate.isSame(day) && day.isSame(moment().startOf('day'))) return 'day today active'
+        else if (selectDate.isSame(day)) return 'day active'
+        else if (day.isSame(moment().startOf('day'))) return 'day today'
+        else if (viewDate.month() !== day.month()) return 'day old'
+        else return 'day'
+    }
+
+    function selectDay(day) {
+
+        console.table({day: day.toDate(), selectDate: selectDate.toDate()});
+        selectDate = day;
+    }
 </script>
 
 <div class="date-picker-container bottom">
-  <div class="date-picker-days">
-    <!-- 放 prev . next icon & 目前年月的地方 -->
-    <div class="date-picker-header">
-      <i class="prev" />
-      <div class="year-month flex-1">
-        <span>2022</span>
-        <span> 年 </span>
-        <span> 12 </span>
-        <span> 月 </span>
-      </div>
-      <i class="next" />
+    <div class="date-picker-days">
+        <!-- 放 prev . next icon & 目前年月的地方 -->
+        <div class="date-picker-header">
+            <div class="icon" on:click={minusMonthToViewDate}>
+                <i class="prev"/>
+            </div>
+            <div class="year-month flex-1 text-center" on:click={console.log}>
+                <span>{viewDate.year()}</span>
+                <span> 年 </span>
+                <span>{viewDate.month() + 1}</span>
+                <span> 月 </span>
+            </div>
+            <div class="icon" on:click={addMonthToViewDate}>
+                <i class="next"/>
+            </div>
+        </div>
+        <!-- 放至內容的地方 -->
+        <div class="date-picker-body">
+            <div class="weekdays">
+                <span>日</span>
+                <span>一</span>
+                <span>二</span>
+                <span>三</span>
+                <span>四</span>
+                <span>五</span>
+                <span>六</span>
+            </div>
+            <div class="days-container">
+                <!-- 總共有 6 行 -->
+                {#each getDateArr(viewDate) as day, i}
+                    <span class="{getDayClass(selectDate,day)}" on:click={() => selectDay(day)}>
+                        {day.date()}
+                    </span>
+                {/each}
+            </div>
+        </div>
     </div>
-    <!-- 放至內容的地方 -->
-    <div class="date-picker-body">
-      <div class="weekdays">
-        <span>日</span>
-        <span>一</span>
-        <span>二</span>
-        <span>三</span>
-        <span>四</span>
-        <span>五</span>
-        <span>六</span>
-      </div>
-      <div class="days-container">
-        <!-- 總共有 6 行 -->
-        <span class="day old">27</span>
-        <span class="day old">28</span>
-        <span class="day">1</span>
-        <span class="day">2</span>
-        <span class="day">3</span>
-        <span class="day active today">4</span>
-        <span class="day">5</span>
-        <span class="day">6</span>
-        <span class="day">7</span>
-        <span class="day">8</span>
-        <span class="day">9</span>
-        <span class="day">10</span>
-        <span class="day">11</span>
-        <span class="day">12</span>
-        <span class="day">13</span>
-        <span class="day">14</span>
-        <span class="day">15</span>
-        <span class="day">16</span>
-        <span class="day">17</span>
-        <span class="day">18</span>
-        <span class="day">19</span>
-        <span class="day">20</span>
-        <span class="day">21</span>
-        <span class="day">22</span>
-        <span class="day">23</span>
-        <span class="day">24</span>
-        <span class="day">25</span>
-        <span class="day">26</span>
-        <span class="day">27</span>
-        <span class="day">28</span>
-        <span class="day">29</span>
-        <span class="day">30</span>
-        <span class="day">31</span>
-        <span class="day old">1</span>
-        <span class="day old">2</span>
-        <span class="day old">3</span>
-        <span class="day old">4</span>
-        <span class="day old">5</span>
-        <span class="day old">6</span>
-        <span class="day old">7</span>
-        <span class="day old">8</span>
-        <span class="day old">9</span>
-      </div>
-    </div>
-  </div>
 </div>
 
 <style lang="scss">
+  .date-picker-container i {
+    height: 12px;
+    width: 12px;
+    display: inline-block;
+    background-size: contain;
+    background-repeat: no-repeat;
+  }
+
+  .date-picker-container .icon {
+    height: 30px;
+    width: 30px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .date-picker-container i.prev {
+    background-image: url("data:image/svg+xml,%3C%3Fxml version='1.0' encoding='iso-8859-1'%3F%3E%3C!-- Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0) --%3E%3Csvg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 55.753 55.753' style='enable-background:new 0 0 55.753 55.753;' xml:space='preserve'%3E%3Cg%3E%3Cpath d='M12.745,23.915c0.283-0.282,0.59-0.52,0.913-0.727L35.266,1.581c2.108-2.107,5.528-2.108,7.637,0.001 c2.109,2.108,2.109,5.527,0,7.637L24.294,27.828l18.705,18.706c2.109,2.108,2.109,5.526,0,7.637 c-1.055,1.056-2.438,1.582-3.818,1.582s-2.764-0.526-3.818-1.582L13.658,32.464c-0.323-0.207-0.632-0.445-0.913-0.727 c-1.078-1.078-1.598-2.498-1.572-3.911C11.147,26.413,11.667,24.994,12.745,23.915z'/%3E%3C/g%3E%3C/svg%3E%0A");
+  }
+
+  .date-picker-container i.next {
+    background-image: url("data:image/svg+xml,%3C%3Fxml version='1.0' encoding='iso-8859-1'%3F%3E%3C!-- Generator: Adobe Illustrator 19.0.0, SVG Export Plug-In . SVG Version: 6.00 Build 0) --%3E%3Csvg version='1.1' xmlns='http://www.w3.org/2000/svg' xmlns:xlink='http://www.w3.org/1999/xlink' x='0px' y='0px' viewBox='0 0 55.752 55.752' style='enable-background:new 0 0 55.752 55.752;' xml:space='preserve'%3E%3Cg%3E%3Cpath d='M43.006,23.916c-0.28-0.282-0.59-0.52-0.912-0.727L20.485,1.581c-2.109-2.107-5.527-2.108-7.637,0.001 c-2.109,2.108-2.109,5.527,0,7.637l18.611,18.609L12.754,46.535c-2.11,2.107-2.11,5.527,0,7.637c1.055,1.053,2.436,1.58,3.817,1.58 s2.765-0.527,3.817-1.582l21.706-21.703c0.322-0.207,0.631-0.444,0.912-0.727c1.08-1.08,1.598-2.498,1.574-3.912 C44.605,26.413,44.086,24.993,43.006,23.916z'/%3E%3C/g%3E%3C/svg%3E%0A");
+  }
+
+  .date-picker-container .text-center {
+    text-align: center;
+  }
+
   .date-picker-container {
     background-color: #fff;
     box-sizing: border-box;
@@ -131,24 +170,50 @@
     }
   }
 
-  .date-picker-body .days-container .day.old {
-    color: #dee2e6;
-  }
-
-  .date-picker-body .days-container .day,
-  .weekdays span {
+  .date-picker-body .days-container .day {
+    position: relative;
     height: 30px;
     line-height: 30px;
     text-align: center;
+
+    &.old {
+      color: #dee2e6;
+    }
+
+    &:hover {
+      background-color: rgba(51, 139, 248, 0.25);
+      cursor: pointer;
+      border-radius: 0.25rem;
+    }
+
+    &.active {
+      font-weight: 700;
+      font-size: 16px;
+      color: #fff;
+      background-color: #60ba9a;
+      border-radius: 50%;
+      padding: 0;
+      transform: scale(0.8);
+    }
+
+    &.active.today::before {
+      border-bottom-color: #fff;
+    }
+
+    &.today::before {
+      content: '';
+      display: inline-block;
+      border: solid transparent;
+      border-width: 0 0 7px 7px;
+      border-bottom-color: #dee2e6;
+      border-top-color: rgba(0, 0, 0, .2);
+      position: absolute;
+      bottom: 4px;
+      right: 4px;
+    }
   }
 
-  .date-picker-body .days-container .day:hover {
-    background-color: rgba(51, 139, 248, 0.25);
-    cursor: pointer;
-    border-radius: 0.25rem;
-  }
-
-  .date-picker-header i:hover,
+  .date-picker-header .icon:hover,
   .date-picker-header .year-month:hover {
     background: #f8f9fa;
     cursor: pointer;
@@ -157,6 +222,14 @@
   .weekdays {
     display: grid;
     grid-template-columns: repeat(7, 30px);
+
+    span {
+      position: relative;
+      height: 30px;
+      line-height: 30px;
+      text-align: center;
+      font-weight: 700;
+    }
   }
 
   .days-container {
@@ -164,26 +237,15 @@
     grid-template-columns: repeat(7, 30px);
   }
 
-  .date-picker-body .days-container .day.active {
-    font-weight: 700;
-    font-size: 16px;
-    color: #fff;
-    background-color: #60ba9a;
-    border-radius: 50%;
-    padding: 0;
-    transform: scale(0.8);
-  }
-
   .date-picker-header {
     display: flex;
     align-items: center;
-  }
-
-  .date-picker-header span {
+    font-weight: 900;
     line-height: 30px;
+    font-size: 16px;
   }
 
-  .flex-1 {
+  .date-picker-container .flex-1 {
     flex-grow: 1;
   }
 </style>
